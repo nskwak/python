71b3583621f1f782cc10e86c4db302e6ccab1b47
root <nskwak@hotmail.com>
2020-02-09 09:58:46 -0800

    20200209: update work.py, add a.txt and then delete a.txt

--- work.py
+++ work.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
 
 import argparse
+import sys
 
 def ParseArgs():
     """Parse command line options into globals."""
     global physDrive, physDriveDict, physDriveTxt, utilization, nullio, isFile
     global outputDest, offset, cluster, yes, quickie, verify, fastPrecond
 
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="A tool to easily run FIO to benchmark sustained "
-        "performance of NVME\nand other types of SSD.",
-        epilog="""
-Requirements:\n
-* Root access (log in as root, or sudo {prog})
-* No filesytems or data on target device
-* FIO IO tester (available https://github.com/axboe/fio)
-* sdparm to identify the NVME device and serial number
-WARNING: All data on the target device will be DESTROYED by this test.""")
+    parser = argparse.ArgumentParser()
 
-    parser.add_argument("--cluster", dest="cluster", action='store_true',
-                        help="Run the test on a cluster (--drive in "+
-                        "host1:/dev/p1,host2:/dev/ps,...)", required=False)
-    parser.add_argument("--verify", dest="verify", action='store_true',
-                        help="Have FIO perform data verifications on reads."+
-                        " May impact performance", required=False)
     parser.add_argument("--drive", "-d", dest="physDrive",
                         help="Device to test (ex: /dev/nvme0n1)", required=True)
+    parser.add_argument("--utilization", "-u", dest="utilization",
+                        help="Amount of drive to test (in percent), 1...100",
+                        default="100", type=int, required=False)
+
     args = parser.parse_args()
 
     physDrive = args.physDrive
-    print("physDrive", physDrive)
+    utilization = args.utilization
+    #print("physDrive", physDrive)
+
+    if (utilization < 1) or (utilization > 100):
+        print("ERROR:  Utilization must be between 1...100")
+        #parser.print_help()
+        sys.exit(1)
 
 
 
@@ -39,7 +34,3 @@ if __name__ == "__main__":
     print("complete!")
 
 
-'''
-print("Hello")
-print("update modified code")
-'''
